44995c33b5c757b5f7ed299ba87676ed0509415e
Matvey0123 <m.kuznetsov4@g.nsu.ru>
2021-03-05 15:40:20 +0700

    task_2_1_1_changed_graph

--- Task_2_1_1/src/main/java/ru/nsu/fit/kuznetsov/primes/Main.java
+++ Task_2_1_1/src/main/java/ru/nsu/fit/kuznetsov/primes/Main.java
@@ -18,16 +18,17 @@ public class Main {
     numbers.add(899, 10);
     Instant before = Instant.now();
     boolean resultSequential = Sequential.isNonPrime(numbers);
-    System.out.println(Duration.between(before, Instant.now()).toMillis() + " ms");
+    System.out.println(
+        Duration.between(before, Instant.now()).toMillis() + " ms" + " - sequentially");
     before = Instant.now();
     try {
-      boolean resultThreads = ConcurrentWithThreads.search(3, numbers);
+      boolean resultThreads = ConcurrentWithThreads.search(16, numbers);
     } catch (Exception e) {
       e.printStackTrace();
     }
-    System.out.println(Duration.between(before, Instant.now()).toMillis() + " ms");
+    System.out.println(Duration.between(before, Instant.now()).toMillis() + " ms" + " - threads");
     before = Instant.now();
     boolean resultStreams = ConcurrentWithStream.search(numbers);
-    System.out.println(Duration.between(before, Instant.now()).toMillis() + " ms");
+    System.out.println(Duration.between(before, Instant.now()).toMillis() + " ms" + " - stream");
   }
 }
