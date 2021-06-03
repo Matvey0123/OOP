f59b61d9f77db3b50c4ab181fc2f4bce1f5142a5
Matvey0123 <m.kuznetsov4@g.nsu.ru>
2021-06-03 14:11:08 +0700

    deleted_clones

--- Task_2_1_2/src/test/java/ru/nsu/fit/kuznetsov/pizza/MainTest.java
+++ Task_2_1_2/src/test/java/ru/nsu/fit/kuznetsov/pizza/MainTest.java
@@ -16,59 +16,42 @@ public class MainTest {
     public void test1() {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream("test.json");
-        assert is != null;
-        Reader reader = new InputStreamReader(is);
 
-        Gson gson = new Gson();
-        JsonReader jsonReader = new JsonReader(reader);
-        Data parameters = gson.fromJson(jsonReader, Data.class);
-        Main.startProcess(parameters);
-
+        start(is);
     }
 
     @Test
     public void testLowCapacity() {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream("testLowCapacity.json");
-        assert is != null;
-        Reader reader = new InputStreamReader(is);
 
-        Gson gson = new Gson();
-        JsonReader jsonReader = new JsonReader(reader);
-        Data parameters = gson.fromJson(jsonReader, Data.class);
-        Main.startProcess(parameters);
+        start(is);
     }
 
     @Test
     public void largeTest() {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream("largeTest.json");
-        assert is != null;
-        Reader reader = new InputStreamReader(is);
 
-        Gson gson = new Gson();
-        JsonReader jsonReader = new JsonReader(reader);
-        Data parameters = gson.fromJson(jsonReader, Data.class);
-        Main.startProcess(parameters);
+        start(is);
     }
 
     @Test
     public void testOneCarrier() {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream("testOneCarrier.json");
-        assert is != null;
-        Reader reader = new InputStreamReader(is);
 
-        Gson gson = new Gson();
-        JsonReader jsonReader = new JsonReader(reader);
-        Data parameters = gson.fromJson(jsonReader, Data.class);
-        Main.startProcess(parameters);
+        start(is);
     }
 
     @Test
     public void testOneBaker() {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream("testOneBaker.json");
+
+        start(is);
+    }
+    private void start(InputStream is){
         assert is != null;
         Reader reader = new InputStreamReader(is);
 
